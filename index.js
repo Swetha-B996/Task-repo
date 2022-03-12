132d9805de9c32f0c4e2d7fd871c0ee286cba29b
root <root@ip-172-31-12-61.ap-south-1.compute.internal>
2022-03-12 15:06:36 +0000

    secondcommit

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const port = process.env.PORT || 5000;
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
-  const msg = 'Hello codincity team!\n'
+  const msg = 'Hello swetha!\n'
   res.end(msg);
 });
 
